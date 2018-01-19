a70aa221936e1954967db46ba17228ef77800c3a
Dmitri Koroza <dmitrik@traiana.com>
2018-01-19 18:01:04 -0500

    print system properties

--- src/main/java/grpc/server/Main.java
+++ src/main/java/grpc/server/Main.java
@@ -14,6 +14,7 @@ public class Main {
 
     public static void main(String[] args) {
         try{
+            System.out.println(System.getProperties());
             ServerBuilder builder = ServerBuilder.forPort(3333);
             builder.addService(parsingService);
             server = builder.build();
